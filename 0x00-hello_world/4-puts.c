d113f4d1efc30399de6c1233655e038e24579a81
mosesobeh1 <mosesobeh@gmail.com>
2022-09-11 10:35:34 -0700

    Puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+
+{
+       // string initializ 
+
+ char str1 [] = "Programming is like building a multilingual puzzle";
+       // stdout char str1
+       
+     puts(str1);
+
+     return (0);
+
+}
