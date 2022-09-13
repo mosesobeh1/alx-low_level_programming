3182913991fd6fb4beb3ba5f1fb2a444d0153555
mosesobeh1 <mosesobeh@gmail.com>
2022-09-13 10:49:01 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *
+ * print alphabets in lower case
+ * Follow aphabets with \n
+ * 
+ * Return O
+ */
+
+void print_alphabet(void)
+
+{ 
+     char c;
+
+     for ( c = 'a'; c<= 'z'; c++)
+        
+     {
+	     _putchar(c);
+
+      } 
+_putchar('\n');
+}
+	     
+  	
