961545b5d7b840f9b5ee434a279a60e8ac735be4
mosesobeh1 <mosesobeh@gmail.com>
2022-09-14 07:24:42 -0700

    Print

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print if the number is positive, zero, or negative
+ *
+ * description: using the main function
+ * this program prints "programming is positive, zero, or negative"
+ * return: 0
+ */
+int main(void)
+{
+char c;
+for  (c = 'z'; c >= 'a' c--)
+{
+putchar(c);
+}
+putchar('\n);
+return (0);
+}
