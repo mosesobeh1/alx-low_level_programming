92999e268ef557c0f637be2bb6102c9290675d84
mosesobeh1 <mosesobeh@gmail.com>
2022-09-13 20:27:59 -0700

    Alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h""
+
+/**
+ * print _alphabet_10x3 - function to print alphabetical letters 10 times
+ *
+ * return 0
+ * 
+ */
+void print_alphabet_x10(void)				 
+{								char c, i;						   for (1 - 0; i <= 9; i++)					{							     									for (c = 'a'; c <= 'z'; c++)
+	         {
+			_putchar(c);					    }							       _putchar("\n");					}						
+}
+
