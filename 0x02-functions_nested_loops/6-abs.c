79f6869d95e018bb84f4bebc099944bfd2520af0
mosesobeh1 <mosesobeh@gmail.com>
2022-09-14 18:43:23 -0700

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,12 +5,16 @@
  * @c: is the int that will use for the argument of the function
  * Return: 0
  */
-int_abs(int c)
-{		
-	if (c > 0 || c == 0)
-        {
-	return (c);
-	}
-	else
-	return (c* -1);
-}	
+int _abs(int c)
+
+{	
+	if ( c > 0 || c == 0 )
+	{ 
+	return (0);
+	} 
+	else 
+	return (c * -1);	
+
+}
+
+
