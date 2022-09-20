b6969561f15da7cb1fadcf7bb4c2cd2da57c580d
mosesobeh1 <mosesobeh@gmail.com>
2022-09-19 18:48:23 -0700

    98 battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reste_98- function that takes a pointer to am int as a parameter and 
+ * updates the value it points to 98
+ *
+ * @n: Enty
+ */
+
+void reset_to_98(int *n)
+{	
+	*n = 98;
+}		
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+void reset_to_98(int *n);
+	void swap_int(int *a, int *b);
+	int _strlen(char *s);
+	void _puts(char *str);
+	int _putchar(char c);
+	void prints_rev(char *s);
+	void puts2(char *str);
+	void puts_half(char *str)
+	void print_array(int *a, int n);
+	char *_strcpy(char *dest, char *scr);
+	void rev_string(char *s);
+	
