fdc6b4bb9414893c0a86ad828cf418a01b5a7e46
mosesobeh1 <mosesobeh@gmail.com>
2022-09-20 04:36:38 -0700

    9.8 battery st

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,8 +3,8 @@
 *reset_98 - function that takes a pointer to an int as a parameter and
 *updates the value it points to 98
 *decription: printout the required result.
-*@n: entry  = 98
-*return:return void
+*@n: entry
+*return: return void
 */
 
 void reset_to_98(int *n)
