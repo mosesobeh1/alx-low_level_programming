27ae6b0730f24a12d02170c88fd5345893977662
mosesobeh1 <mosesobeh@gmail.com>
2022-09-20 10:15:16 -0700

    battery st

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,13 @@
-#include < main.h >
+#include "main.h"
 
 /**
-* reset_to_98 - resets the value of the variable.
-* it is pointing to to 98.
-* @n: argument to the function.
-* Description: print out the required result.
-* Return: return void.
+*reset_to_98 - resets the value of the variable.
+*it is pointing to to 98.
+*@n: argument to the function.
+*Description: print out the required result.
+*Return: return void.
 */
-
-void reset_to_98(int *n)
-{
-		 *n = 98;
+void reset_to_98(int*n)
+{	
+	*n = 98;
 }
