95c359310b0f6017e8bb60e61acb915baaf85cbf
mosesobeh1 <mosesobeh@gmail.com>
2022-09-20 03:30:45 -0700

    Battery st

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,12 +1,11 @@
-void reset_to_98(int *n);
-	void swap_int(int *a, int *b);
-	int _strlen(char *s);
-	void _puts(char *str);
-	int _putchar(char c);
-	void prints_rev(char *s);
-	void puts2(char *str);
-	void puts_half(char *str)
-	void print_array(int *a, int n);
-	char *_strcpy(char *dest, char *scr);
-	void rev_string(char *s);
-	
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
+#endif /* MAIN_H */
+
