02883a43711ba499b955e94242ef84bdb9619a8c
mosesobeh1 <mosesobeh@gmail.com>
2022-09-21 04:17:05 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,6 +8,14 @@ void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 void _puts(char *str);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif /* MAIN_H */
 
