1fab32c81b9dfb58a3bb89a6697ad1bb502d4964
mosesobeh1 <mosesobeh@gmail.com>
2022-09-21 01:02:09 -0700

    Swap

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,6 +7,7 @@ int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 void _puts(char *str);
+void swap_int(int *a, int *b);
 
 #endif /* MAIN_H */
 
