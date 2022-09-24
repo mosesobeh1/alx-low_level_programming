36c819006ceab127ca8d591ba6ad05a0f9a3c640
mosesobeh1 <mosesobeh@gmail.com>
2022-09-24 07:44:13 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the number since  0 - 9
+ *
+ * Return: Always (success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
