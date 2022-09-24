8556082542906b48871d7e3a05934148dfe88130
mosesobeh1 <mosesobeh@gmail.com>
2022-09-24 08:01:42 -0700

    Rverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print lowerscase alphabet in reverse
+ *
+ * Return: Always(success)
+ */
+int main(void)
+{
+	char v;
+
+	for (v = 'z'; v <= 'a'; c--)
+	{
+		putchar(v);
+	}
+	putchar('\n');
+	return (0);
+}
