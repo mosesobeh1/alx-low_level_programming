d668bf65076ff671d6340d047fc51d9b77b8de27
mosesobeh1 <mosesobeh@gmail.com>
2022-09-24 02:02:45 -0700

    Printing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
+ * main - printing
  *
- * print the string using the put function
- *
- * main returns 0
+ * Return: always 0 (Successful)
  */
 int main(void)
 {
-     puts("\'Programming is like building a multilingual puzzle");
-     return (0);
-}     
+	 puts("\" Programming is like building a multilingual puzzle");
+
+	return (0);
+}
