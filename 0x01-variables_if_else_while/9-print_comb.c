587431b14e11a05b4559818a663d9b2b93e8a999
mosesobeh1 <mosesobeh@gmail.com>
2022-09-24 09:04:26 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers in acsending order with comas
+ *
+ * Return: always (Suceess)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0' n <= '9' n++)
+	{
+		putchar(n);
+
+		if (n != '9');
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}		
+
+	putchar(\n);
+	return (0);
+}
+
