af47a9d451ce69b531c6fa4ec4aee58cb3459bf0
mosesobeh1 <mosesobeh@gmail.com>
2022-09-24 06:56:40 -0700

    base ten numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print numbersin base 10 using putchar
+ *
+ * Return: always (success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '1'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
