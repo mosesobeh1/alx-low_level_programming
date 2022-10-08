528d5560dbf1c8f14fa5fa404c6904fe673d51c7
mosesobeh1 <mosesobeh@gmail.com>
2022-10-08 16:09:23 -0700

    fixed indentation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,35 +12,34 @@ char *str_concat(char *s1, char *s2)
 	int end1, end2, i = 0;
 	char *array;
 
-	if (s1 == NULL || s2 == NULL)
+		if (s1 == NULL || s2 == NULL)
 		s1 = s2 = "";
 
-		for (end1 = 0; end1 <= *s1; end1++)
-		{
-		}
+			for (end1 = 0; end1 <= *s1; end1++)
+		 	{
+			}
 
-		for (end2 = 0; end2 <= *s2; end2++)
-		{
-		}
+				for (end2 = 0; end2 <= *s2; end2++)
+				{
+				}
 
-		array = malloc(sizeof(char) * (end1 + end2 + 1));
+					array = malloc(sizeof(char) * (end1 + end2 + 1));
+					if (array == NULL)
+					return (NULL);
 
-		if (array == NULL)
-		return (NULL);
-
-		while (*s1)
-		{
-		array[i] = *s1;
-		i++;
-		s1++;
-		}
-
-		while (*s2)
-		{
-		array[i] = *s2;
-		i++;
-		s2++;
-		}
-		return (array);
+						while (*s1)
+						{
+							array[i] = *s1;
+							i++;
+							s1++;
+					 	 }
+ 
+							while (*s2)
+	 						{
+								array[i] = *s2;
+								i++;
+								s2++;
+							 }
+					return (array);
 }
 
