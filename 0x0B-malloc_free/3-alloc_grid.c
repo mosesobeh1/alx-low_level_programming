52a2aec3fe1636bfb9914c36ec1ca5fbdc8f6b10
mosesobeh1 <mosesobeh@gmail.com>
2022-10-11 03:02:35 -0700

    malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,33 +9,31 @@
  */
 int **alloc_grid(int width, int height)
 {
-		int **iarray;
-			int i, n;
+	int **iarray;
+	int i, n;
 
-				if (width <= 0 || height <= 0)
-							return (NULL);
+	if (width <= 0 || height <= 0)
+	return (NULL);
 
-					iarray = malloc(sizeof(int) * height);
+	iarray = malloc(sizeof(int) * height);
 
-						if (iarray == NULL)
-								{
-											free(iarray);
-													return (NULL);
-														}
+	if (iarray == NULL)	
+		{
+			free(iarray);
+																return (NULL);
+		}
 
-							for (i = 0; i < height; i++)
-									{
-												iarray[i] = malloc(sizeof(int) * width);
-														if (iarray == NULL)
-																	{
-																					free(iarray);
-																								return (NULL);
-																										}
-
-																for (n = 0; n < width; n++)
-																			{
-																							iarray[i][n] = 0;
-																									}
-																	}
-								return (iarray);
+			for (i = 0; i < height; i++)
+		{
+			iarray[i] = malloc(sizeof(int) * width);
+				if (iarray == NULL)
+				{
+					free(iarray);
+																		return (NULL);
+																	}														for (n = 0; n < width; n++)
+																		{
+																			iarray[i][n] = 0;
+																		}	
+		}
+					return (iarray);
 }
